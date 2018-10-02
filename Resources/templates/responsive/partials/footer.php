88b1bb138c80c5c2c4ecbe21effc0046fec06951
Teslacoin <teslacoinfoundation@gmail.com>
2018-10-02 18:01:29 -0500

    Footer
    Remove facebook, instagram and newsletter

--- Resources/templates/responsive/partials/footer.php
+++ Resources/templates/responsive/partials/footer.php
@@ -22,11 +22,8 @@
         </div>
         <div class="social text-center">
           <a class="fa fa-twitter" title="" target="_blank" href="<?=$this->text('social-account-twitter') ?>"></a>
-          <a class="fa fa-facebook" title="" target="_blank" href="<?=$this->text('social-account-facebook') ?>"></a>
-          <a class="fa fa-instagram" title="" target="_blank" href="<?=$this->text('social-account-instagram') ?>"></a>
           <a class="fa fa-telegram" title="" target="_blank" href="<?=$this->text('social-account-telegram') ?>"></a>
           <a class="fa fa-github" title="" target="_blank" href="<?=$this->text('social-account-github') ?>"></a>
-          <a class="fa fa-newspaper-o" title="" target="_blank" href="/newsletter"></a>
         </div>
       </div>
     </div>
