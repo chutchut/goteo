67f5173db35e32ec07aa5a27005bbd3fd9bd9918
Calum Hutton <c.e.hutton@gmx.com>
2018-10-03 00:30:08 +0100

    Populate call-to-action partial (buy tes)

---
+++ extend/tes-starter/Resources/templates/responsive/home/partials/call_to_action.php
@@ -0,0 +1,19 @@
+<div class="section call-action">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-md-7">
+                <div class="title">
+                Crowdfunding free energy
+                </div>
+                <div class="description">
+                    Buy TeslaCoin and collaborate with your favourite projects!<br>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-3 col-md-offset-1 col-button">
+                <a target="_blank" href="https://www.cryptopia.co.nz/Exchange/?market=TES_BTC" class="btn btn-white">
+                	Buy now
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
