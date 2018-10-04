d2fb1c5ff3971f7ceda183c460472d8d3dff7270
Teslacoin <teslacoinfoundation@gmail.com>
2018-10-03 19:27:13 -0500

    Footer - TeslaStarter v.0.1
    Change Goteo logo for TeslaStarter v0.1 text

--- Resources/templates/responsive/partials/footer.php
+++ Resources/templates/responsive/partials/footer.php
@@ -16,7 +16,7 @@
             <div class="col-sm-4 hidden-xs">
               <ul class="list-inline text-right">
                 <li class="label-img"><a href="#"><?=$this->text('footer-platoniq-iniciative') ?></a></li>
-                <li><a href="/blog"><img src="/view/css/logoFG.png" class="img-responsive logo" alt="Fundación Goteo"></a></li>
+                <li> TeslaStarter v0.1 </li>
               </ul>
             </div>
         </div>
